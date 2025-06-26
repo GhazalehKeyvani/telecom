fb92b1e7b9136417ef0992e22ea02aa12131171e
GhazalehKeyvani <ghkey278@gmail.com>
2025-06-26 12:57:37 +0330

    delet

--- llm_agents/main.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = "http://103.75.197.35:11434"
-headers = {"Content-Type": "application/json"}
-data = {"prompt": "سلام، دنیای LLM"}
-
-response = requests.post(url, json=data, headers=headers)
-print("Status Code:", response.status_code)
-print("Response:", response.text)
